527c37df3d39a67297f0b91205227f5c9c862ea8
narcisdiaconu <narcis.diaconu@info.uaic.ro>
2019-05-19 12:33:41 +0300

    Initial application generated by JHipster-5.8.2

---
+++ Microservices/tickets-app/src/main/java/com/busticketbooking/tickets/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.busticketbooking.tickets.config.audit;
---
+++ Microservices/tickets-app/src/main/java/com/busticketbooking/tickets/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.busticketbooking.tickets.web.rest.vm;
